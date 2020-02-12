6ee28c4d3e64ec977637e53c873771ea5c083f54
George Bisbas <g.bisbas18@imperial.ac.uk>
2020-02-12 14:24:15 +0000

    Clean init

--- shipyard-config/devitobench_poc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This bash scripts takes 2 arguments:
-# Arg1 is installation path of shipyard
-# Arg2 is the crentials file name
-
-shipyard pool add --credentials $1
-shipyard jobs add --credentials $1
